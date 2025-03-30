65e722de808bf6c1eab8d880332cf40a8916229b
zura <makaradze98.s@gmail.com>
2025-03-30 01:45:31 -0400

    s

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -15,14 +15,20 @@ const MasonryGridExample = () => {
     setLoading(true);
     
     const imageUrls = [
+      'https://res.cloudinary.com/dzvttwdye/image/upload/v1743311725/7b4328da-7486-4298-9ef6-29c38a433342_ybwbsk.jpg',
+      'https://res.cloudinary.com/dzvttwdye/image/upload/v1743311677/download_13_yna1rl.png',
+      'https://res.cloudinary.com/dzvttwdye/image/upload/v1743311677/download_14_vyfk4m.png',
+      'https://res.cloudinary.com/dzvttwdye/image/upload/v1743295994/anime_yfap6y.png',
       'https://res.cloudinary.com/dzvttwdye/image/upload/v1743295994/main-2_w8ldom.png',
       'https://res.cloudinary.com/dzvttwdye/image/upload/v1743295995/main-1_ogj0zg.png',
+      'https://res.cloudinary.com/dzvttwdye/image/upload/v1743313392/download_18_o1ak1c.png',
+      'https://res.cloudinary.com/dzvttwdye/image/upload/v1743304461/2313_mr9leg.png',
+      'https://res.cloudinary.com/dzvttwdye/image/upload/v1743308123/download_6_mwv31t.png',
+      'https://res.cloudinary.com/dzvttwdye/image/upload/v1743307006/o2rxody4sugd6aydrgtl.png',
       'https://res.cloudinary.com/dzvttwdye/image/upload/v1743295995/Screenshot_2025-03-29_191414_cwkvsf.png',
-      'https://res.cloudinary.com/dzvttwdye/image/upload/v1743295994/Screenshot_2025-03-29_190345_gk19da.png',
-      'https://res.cloudinary.com/dzvttwdye/image/upload/v1743302988/download_2_ilkhu1.png',
-      'https://res.cloudinary.com/dzvttwdye/image/upload/v1743303850/download_d9zxel.png',
-      'https://res.cloudinary.com/dzvttwdye/image/upload/v1743295994/anime_yfap6y.png',
-      'https://res.cloudinary.com/dzvttwdye/image/upload/v1743304461/2313_mr9leg.png'
+ 
+
+
     ];
     
     const newData = imageUrls.map((src, i) => ({
