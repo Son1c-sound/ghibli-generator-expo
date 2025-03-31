6e5f8a1307dfbe8f0887b27dd20925602689ea4e
zura <makaradze98.s@gmail.com>
2025-03-30 23:16:54 -0400

    s

--- app/utils/stylesData.ts
+++ app/utils/stylesData.ts
@@ -5,12 +5,12 @@ export const stylesList: StyleItem[] = [
   { 
     id: 1, 
     name: 'Anime', 
-    src: 'https://res.cloudinary.com/dzvttwdye/image/upload/v1743313391/download_17_bxmkps.png' 
+    src: 'https://res.cloudinary.com/dzvttwdye/image/upload/v1743390806/cooylozjk3ssauku8jer.png' 
   },
   { 
     id: 2, 
     name: 'Ghibli', 
-    src: 'https://res.cloudinary.com/dzvttwdye/image/upload/v1743295995/Screenshot_2025-03-29_190425_pqmti9.png' 
+    src: 'https://res.cloudinary.com/dzvttwdye/image/upload/v1743311677/download_17_dowitk.png' 
   },
   { 
     id: 33, 
@@ -36,7 +36,7 @@ export const stylesList: StyleItem[] = [
   { 
     id: 6, 
     name: 'PixelArt', 
-    src: 'https://res.cloudinary.com/dzvttwdye/image/upload/v1743386803/enoug6govsv3ekuw4l8a.png' 
+    src: 'https://res.cloudinary.com/dzvttwdye/image/upload/v1743390979/fxtcvex3j8ncjbmksrod.png' 
   },
   { 
     id: 9, 
