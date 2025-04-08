c129645c8a71d1f05bfb0444161cdafd1f99961b
zura <makaradze98.s@gmail.com>
2025-04-08 06:14:09 -0400

    s

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,7 +6,7 @@ import { Svg, Path, Circle, G } from 'react-native-svg';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-const HomeIcon = ({ color }) => (
+const HomeIcon = ({ color }: {color:any}) => (
   <Svg width={28} height={28} viewBox="0 0 24 24" fill="none">
     <Path
       d="M2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C22 4.92893 22 7.28595 22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12Z"
@@ -24,7 +24,7 @@ const HomeIcon = ({ color }) => (
   </Svg>
 );
 
-const ChatIcon = ({ color }) => (
+const ChatIcon = ({ color }: {color:any}) => (
   <Svg width={28} height={28} viewBox="0 0 24 24" fill="none">
     <Path d="M12 16V8" stroke={color} strokeWidth="1.5" strokeLinecap="round" />
     <Path d="M8 14V10" stroke={color} strokeWidth="1.5" strokeLinecap="round" />
@@ -38,7 +38,7 @@ const ChatIcon = ({ color }) => (
   </Svg>
 );
 
-const ProfileIcon = ({ color }) => (
+const ProfileIcon = ({ color }: {color:any}) => (
   <Svg width={28} height={28} viewBox="0 0 24 24" fill="none">
     <Path 
       d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" 
